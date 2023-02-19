6d41ab81b1ddebc983c5d5d7ca9480425c654fae
Hongdae Kim <mongmeo.dev@gmail.com>
2023-02-19 11:15:16 +0900

    [Feat] : SchedulerConfiguration에 Async 설정 추가

--- src/main/java/shop/yesaladin/socket/config/SchedulerConfiguration.java
+++ src/main/java/shop/yesaladin/socket/config/SchedulerConfiguration.java
@@ -1,12 +1,40 @@
 package shop.yesaladin.socket.config;
 
+import java.util.concurrent.Executor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
+/**
+ * 스케쥴러와 관련된 설정을 정의하는 configuration 클래스입니다.
+ *
+ * @author 김홍대
+ * @since 1.0
+ */
 @Configuration
 @EnableAsync
 @EnableScheduling
-public class SchedulerConfiguration {
+public class SchedulerConfiguration implements AsyncConfigurer, SchedulingConfigurer {
 
+    public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(Runtime.getRuntime().availableProcessors() * 2);
+        scheduler.setThreadNamePrefix("SOCKET-SCHEDULER-");
+        scheduler.initialize();
+        return scheduler;
+    }
+
+    @Override
+    public Executor getAsyncExecutor() {
+        return this.threadPoolTaskScheduler();
+    }
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        taskRegistrar.setTaskScheduler(this.threadPoolTaskScheduler());
+    }
 }
